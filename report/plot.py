1f7afc1257e648fe6ec21f25b32f8486c6cce1f1
Jacarte <javierca@kth.se>
2020-10-09 21:58:36 +0200

    Adding plot for binary reduction

--- report/plot.py
+++ report/plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 blocking = [0.168, 0.0821, 0.0474, 0.0287, 0.0184]
+binary_tree_blocking = [0.07, 0.042, 0.022, 0.011, 0.0092]
 non_blocking_data = [0.077699, 0.044840, 0.022413, 0.012153, 0.009659]
 gather = [0.0714, 0.0380, 0.0271, 0.0124, 0.0082]
 collective = [0.080, 0.0455, 0.0197, 0.0116, 0.0087]
@@ -12,6 +13,16 @@ plt.ylabel("execution time(s)")
 plt.plot([8, 16, 32, 64, 128], blocking, 'o-')
 plt.savefig("graph-pi-blocking.png", dpi=800)
 plt.cla()
+
+
+plt.title("Blocking communication with binary tree reduction")
+plt.grid(True)
+plt.xlabel("processes")
+plt.ylabel("execution time(s)")
+plt.plot([8, 16, 32, 64, 128], binary_tree_blocking, 'o-')
+plt.savefig("graph-pi-bin-blocking.png", dpi=800)
+plt.cla()
+
 
 plt.title("Non blocking communication")
 plt.grid(True)
@@ -44,11 +55,12 @@ plt.grid(True)
 plt.xlabel("processes")
 plt.ylabel("execution time(s)")
 plt.plot([8, 16, 32, 64, 128], blocking, 'o-')
+plt.plot([8, 16, 32, 64, 128], binary_tree_blocking, 'o-')
 plt.plot([8, 16, 32, 64, 128], non_blocking_data, 'o-')
 plt.plot([8, 16, 32, 64, 128], gather, 'o-')
 plt.plot([8, 16, 32, 64, 128], collective, 'o-')
 
-plt.legend(["Blocking communication", "Non blocking communication", "MPI_Gather", "MPI_Reduce"])
+plt.legend(["Blocking communication","Blocking communication with binary tree reduction", "Non blocking communication", "MPI_Gather", "MPI_Reduce"])
 plt.savefig("all.png", dpi=800)
 
 #plt.xticks([8,16,32,64,128])
